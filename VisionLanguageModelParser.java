e69fafcf6c3d897d04f4eb1477bfc98112214041
Salamastik <salamastik@gmail.com>
2025-10-19 14:44:02 +0300

    Update VisionLanguageModelParser.java

--- VisionLanguageModelParser.java
+++ VisionLanguageModelParser.java
@@ -240,6 +240,7 @@ public class VisionLanguageModelParser extends AbstractParser {
         try {
             LOGGER.info("start");
             // String analysis = callVisionAPI(base64Image, mimeType);
+            analysis = "HI VISION";
             XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, metadata);
             xhtml.startDocument();
             

1209f02303f23296bce2e5ca1ed0c32ab2d5dcb8
Salamastik <salamastik@gmail.com>
2025-10-19 15:21:31 +0300

    Refactor parse method and clean up code

--- ParallelizingParserDecorator.java
+++ ParallelizingParserDecorator.java
@@ -3,7 +3,9 @@ package org.apache.tika.parallel;
 
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.*;
+import org.apache.tika.parser.DefaultParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.ParserDecorator;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
@@ -11,29 +13,25 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ParallelizingParserDecorator extends ParserDecorator {
-
-  public ParallelizingParserDecorator() {
-    super(new DefaultParser());
-  }
-
-  @Override
-  public void parse(InputStream stream, ContentHandler handler, Metadata metadata, ParseContext context)
-      throws IOException, SAXException, TikaException {
-
-    // הזרקת ה-Factory כך שכל embedded ילך דרך המקבילי
-    context.set(org.apache.tika.extractor.EmbeddedDocumentExtractorFactory.class,
-                new org.apache.tika.parallel.ParallelEmbeddedDocumentExtractorFactory());
-
-    try {
-      super.parse(stream, handler, metadata, context);
-    } finally {
-      // ניקוז התוצאות ל-handler הראשי (בטוח-Thread)
-      var ex =org.apache.tika.parallel.ParallelEmbeddedDocumentExtractorFactory.CURRENT.get();
-      if (ex != null) {
-        ex.drainTo(handler, metadata);
-        org.apache.tika.parallel.ParallelEmbeddedDocumentExtractorFactory.CURRENT.remove();
-      }
+    public ParallelizingParserDecorator() {
+        super(new DefaultParser());
     }
-  }
+
+    @Override
+    public void parse(InputStream stream, ContentHandler handler,
+                      Metadata metadata, ParseContext context)
+            throws IOException, SAXException, TikaException {
+
+        // מזריקים את ה-Factory שלנו לפרסינג של embedded
+        context.set(org.apache.tika.extractor.EmbeddedDocumentExtractorFactory.class,
+                new ParallelEmbeddedDocumentExtractorFactory());
+
+        try {
+            super.parse(stream, handler, metadata, context);
+        } finally {
+            // נקודת הסיום: לכתוב את בלוקי ה-XHTML שסיכמנו מכל התמונות
+            ParallelEmbeddedDocumentExtractorFactory.drainTo(handler);
+        }
+    }
 }
 

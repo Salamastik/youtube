469968185a949624bec91f3b8a9e7edd714f6c89
Salamastik <salamastik@gmail.com>
2025-10-19 16:04:26 +0300

    Inject XHTML blocks before closing body tag

--- ParallelizingParserDecorator.java
+++ ParallelizingParserDecorator.java
@@ -13,6 +13,10 @@ import org.xml.sax.SAXException;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * עוטף שמזריק את ה-Factory לפרסינג של embedded,
+ * ומזריק בלוקי XHTML לתוך <body> ממש לפני שסוגרים אותו.
+ */
 public class ParallelizingParserDecorator extends ParserDecorator {
     public ParallelizingParserDecorator() {
         super(new DefaultParser());
@@ -23,22 +27,27 @@ public class ParallelizingParserDecorator extends ParserDecorator {
                       Metadata metadata, ParseContext context)
             throws IOException, SAXException, TikaException {
 
-        // מזריקים את ה-Factory שיריץ embedded בפרלליות ויצבור תוצאות
+        // ה-Factory שמריץ תמונות במקביל ואוסף Metadata לכל תמונה:
         context.set(org.apache.tika.extractor.EmbeddedDocumentExtractorFactory.class,
                 new ParallelEmbeddedDocumentExtractorFactory());
 
-        // עוטפים את ה-handler כדי להזריק פלט רגע לפני endDocument()
+        // עוטפים את ה-handler כדי "להתפרץ" רגע לפני שסוגרים את <body>
         ContentHandler injectingHandler = new ContentHandlerDecorator(handler) {
+            private boolean injected = false;
+
             @Override
-            public void endDocument() throws SAXException {
-                // נכתוב את בלוקי ה-XHTML לפני הסגירה
-                // שימי לב: מעבירים את ה-decorator עצמו, לא getContentHandler()
-                ParallelEmbeddedDocumentExtractorFactory.drainTo(this);
-                super.endDocument();
+            public void endElement(String uri, String localName, String qName) throws SAXException {
+                // לפני שסוגרים את <body> פעם ראשונה – נזריק את הבלוקים
+                if (!injected && "body".equals(localName)) {
+                    ParallelEmbeddedDocumentExtractorFactory.drainTo(this);
+                    injected = true;
+                }
+                super.endElement(uri, localName, qName);
             }
         };
 
-        // פרסינג עם ה-handler העטוף
+        // להריץ את ה-parser האמיתי עם ה-handler העטוף
         super.parse(stream, injectingHandler, metadata, context);
     }
 }
+
